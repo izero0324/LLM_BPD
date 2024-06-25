7ed74e22f899c392788ee70c5df43d30d99262d8
izero0324 <andrew1997tw24@gmail.com>
2024-06-25 14:58:18 +0100

    argumented main function

--- benchmark.py
+++ benchmark.py
@@ -1,39 +1,59 @@
+import argparse
 from tools.extract import get_data, extract_code
 from tools.python_exec import test_code
 from LLMs.LLM_api import LLM_revise
 
 
-dataset = get_data("few-shot")
-accuracy = 0 
-boost= 0 
-counter= 0
-flake8 = 0
+def benchmark_process(dataset, model, debug = False):
+    accuracy = 0 
+    boost= 0 
+    flake8 = 0
 
-debug = 1
+    for test_data in dataset:
+        code = extract_code(test_data)
+        # original run
+        success, runtime, error, flake8_error = test_code(test_data)
+        
+        #pass code to LLM for optimize
+        test_data['code'] = LLM_revise(code, model = model)
+        LLM_success, LLM_runtime, LLM_error, LLM_flake8_error = test_code(test_data)
 
-for test_data in dataset:
-    code = extract_code(test_data)
-    # original run
-    success, runtime, error, flake8_error = test_code(test_data)
+        accurate = (LLM_success==1)
+        effi_boost = (runtime - LLM_runtime)/LLM_runtime > 0  # Modify to % with threashold
+        
+        accuracy += accurate
+        boost += effi_boost
+        flake8 += int(flake8_error) - int(LLM_flake8_error)
+        if debug:
+            print("Original cade: ", success, runtime, error, flake8_error)
+            print("ChatGPT: ",LLM_success, LLM_runtime, LLM_error, LLM_flake8_error)
+        # counter += 1
+        # if counter%10 == 0:
+        #     print(counter/10, "0%", " done")
+
+
+    accuracy /= len(dataset) 
+
+    print("accuracy: ",accuracy * 100,"Code boosted: ", boost, "flake8 fixed: ", flake8)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Run an experiment with specified parameters.')
+    parser.add_argument('--data', type=str, default="few-shot", help='dataset name')
+    parser.add_argument('--model', type=str, default='GPT3.5', help='model name.')
+    parser.add_argument('--debug', type=bool, default=False, help='Debug mode')
+
+    return parser.parse_args()
+
+
+def main():
+    '''
+    Entry point
+    '''
+    args = parse_arguments()
+    benchmark_process(dataset= get_data(args.data), model=args.model, debug=args.debug)
     
-    #pass code to LLM for optimize
-    test_data['code'] = LLM_revise(code, model= 'naive RAG')
-    LLM_success, LLM_runtime, LLM_error, LLM_flake8_error = test_code(test_data)
 
-    accurate = (LLM_success==1)
-    effi_boost = (runtime - LLM_runtime)/LLM_runtime > 0  # Modify to % with threashold
-    
-    accuracy += accurate
-    boost += effi_boost
-    flake8 += int(flake8_error) - int(LLM_flake8_error)
-    if debug:
-        print("Original cade: ", success, runtime, error, flake8_error)
-        print("ChatGPT: ",LLM_success, LLM_runtime, LLM_error, LLM_flake8_error)
-    # counter += 1
-    # if counter%10 == 0:
-    #     print(counter/10, "0%", " done")
-
-
-accuracy /= len(dataset) 
-
-print("accuracy: ",accuracy * 100,"Code boosted: ", boost, "flake8 fixed: ", flake8)
+if __name__ == '__main__':
+    print("Start Python code optimize evaluation benchmark...")
+    main()
