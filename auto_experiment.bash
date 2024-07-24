76feee89d921220a0766729dfa12c47df1abb30c
izero0324 <andrew1997tw24@gmail.com>
2024-07-24 13:30:11 +0100

    retrieval choose

---
+++ auto_experiment.bash
@@ -0,0 +1,3 @@
+date 
+python3 benchmark.py --data 'train' --model 'codellama-13b' --debug True > logs/EX1_PureLLM_codellama13b.txt
+date
--- tools/retrieval.py
+++ tools/retrieval.py
@@ -13,6 +13,16 @@ def retrive_docs(input_code, k=3):
         [snippet['metadata']['code'] for snippet in retrieved_snippets['matches']])
     
     return retireval_prompt
+
+def retrive_codes(input_code, k=3):
+    _, index = pc_connect('python-18k-instructions')
+    embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v2')
+    input_embedding = embedding_model.encode(input_code).tolist()
+    retrieved_snippets = index.query(vector=[input_embedding], top_k=k, include_metadata=True)
+    retireval_prompt ="\n --- \n  Related optimisations: " + " ".join(
+        [snippet['metadata']['output'] for snippet in retrieved_snippets['matches']])
+    
+    return retireval_prompt
  
 
  
